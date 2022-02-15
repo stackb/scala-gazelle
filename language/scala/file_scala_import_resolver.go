5726285d564d49137719bc93a894d7bb366e2cea
Paul Johnston <paul.johnston02@sap.com>
2022-02-14 17:52:42 -0700

    Implement cross-resolve for jarindex files

--- language/scala/file_scala_import_resolver.go
+++ language/scala/file_scala_import_resolver.go
@@ -3,22 +3,29 @@ package scala
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/bazelbuild/bazel-gazelle/config"
+	"github.com/bazelbuild/bazel-gazelle/label"
 	"github.com/bazelbuild/bazel-gazelle/resolve"
 
 	"github.com/stackb/scala-gazelle/pkg/index"
 )
 
 func init() {
-	CrossResolvers().MustRegisterCrossResolver("stackb:scala-gazelle:file", &fileScalaImportResolver{})
+	CrossResolvers().MustRegisterCrossResolver("stackb:scala-gazelle:file", &fileScalaImportResolver{
+		byLabel: make(map[string][]label.Label),
+	})
 }
 
+// fileScalaImportResolver provides a cross-resolver for precompiled symbols that are
+// provided by the mergeindex tool.
 type fileScalaImportResolver struct {
 	// indexFile is the filesystem path to the index.
 	indexFile string
-	// index is the spec
-	index *index.IndexSpec
+	// byLabel is a mapping from an import string to the label that provides it.
+	// It is possible more than one label provides a class.
+	byLabel map[string][]label.Label
 }
 
 // RegisterFlags implements part of the ConfigurableCrossResolver interface.
@@ -36,11 +43,36 @@ func (r *fileScalaImportResolver) CheckFlags(fs *flag.FlagSet, c *config.Config)
 	if err != nil {
 		return fmt.Errorf("error while reading index specification file %s: %v", r.indexFile, err)
 	}
-	r.index = index
+
+	for _, jarSpec := range index.JarSpecs {
+		for _, class := range jarSpec.Classes {
+			jarLabel, err := label.Parse(jarSpec.Label)
+			if err != nil {
+				log.Println("bad label while loading jar spec %s: %v", jarSpec.Filename, err)
+				continue
+			}
+			r.byLabel[class] = append(r.byLabel[class], jarLabel)
+		}
+	}
+
 	return nil
 }
 
 // CrossResolve implements the CrossResolver interface.
 func (r *fileScalaImportResolver) CrossResolve(c *config.Config, ix *resolve.RuleIndex, imp resolve.ImportSpec, lang string) []resolve.FindResult {
-	return nil
+	if lang != "scala" {
+		return nil
+	}
+
+	resolved := r.byLabel[imp.Imp]
+	if len(resolved) == 0 {
+		return nil
+	}
+
+	result := make([]resolve.FindResult, len(resolved))
+	for i, v := range resolved {
+		result[i] = resolve.FindResult{Label: v}
+	}
+
+	return result
 }
