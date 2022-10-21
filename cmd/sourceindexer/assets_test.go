730407b18ec61aedfd262fe87aa0a2a101ff9b40
Paul Johnston <paul.johnston02@sap.com>
2022-10-20 19:02:31 -0600

    Checkpoint cmd/sourceindexer tests

---
+++ cmd/sourceindexer/assets_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestEmbed(t *testing.T) {
+	listFiles(".")
+	if len(sourceindexerJs) == 0 {
+		t.Error("embedded sourceindexer.js script is missing")
+	}
+	if len(scalametaParsersIndexJs) == 0 {
+		t.Error("embedded node_modules/scalameta-parsers/index.js script is missing")
+	}
+	if len(nodeExe) != 76198080 {
+		t.Errorf("embedded @build_bazel_rules_nodejs//toolchains/node:node_bin is missing: len %d", len(nodeExe))
+	}
+}
--- cmd/sourceindexer/sourceindexer.go
+++ cmd/sourceindexer/sourceindexer.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"embed"
-	"flag"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -11,79 +8,26 @@ import (
 	"syscall"
 )
 
-var assets embed.FS
-
-type options struct {
-	debug                bool
-	restoreEmbeddedFiles bool
-	nodeBinPath          string
-	nodePath             string
-	scriptPath           string
-	files                []string
-}
-
-func parseOptions(args []string) (*options, error) {
-	var opts options
-	var fs flag.FlagSet
-	fs.StringVar(&opts.nodeBinPath, "node_bin_path", "", "override location of node binary")
-	fs.StringVar(&opts.nodePath, "node_path", "", "override location of node modules")
-	fs.StringVar(&opts.scriptPath, "script_path", "", "override location of sourceindexer script")
-	fs.BoolVar(&opts.restoreEmbeddedFiles, "embedded", true, "restore embedded files")
-	fs.BoolVar(&opts.debug, "debug", false, "debug mode")
-	if err := fs.Parse(args[1:]); err != nil {
-		return nil, err
-	}
-	opts.files = fs.Args()
-
-	if opts.restoreEmbeddedFiles {
-		tmpDir := os.TempDir()
-		// mustRestore(&opts, tmpDir, embeddedInterpreter)
-		// mustRestore(&opts, tmpDir, embeddedAssets)
-		if opts.nodeBinPath == "" {
-			opts.nodeBinPath = filepath.Join(tmpDir, "external/nodejs_darwin_amd64/bin/nodejs/bin/node")
-		}
-		if opts.nodePath == "" {
-			opts.nodePath = filepath.Join(tmpDir, "cmd/sourceindexer")
-		}
-		if opts.scriptPath == "" {
-			opts.scriptPath = filepath.Join(tmpDir, "cmd/sourceindexer/sourceindexer.js")
-		}
-	}
-
-	// in the case where this tool is being run as an action the paths are
-	// relative to the runfiles dir.
-	runfilesDir := args[0] + ".runfiles"
-	if opts.nodeBinPath == "" {
-		opts.nodeBinPath = filepath.Join(runfilesDir, "nodejs_darwin_amd64/bin/nodejs/bin/node")
-	}
-	if opts.nodePath == "" {
-		opts.nodePath = filepath.Join(runfilesDir, "scala_gazelle/cmd/sourceindexer")
-	}
-	if opts.scriptPath == "" {
-		opts.scriptPath = filepath.Join(runfilesDir, "scala_gazelle/cmd/sourceindexer/sourceindexer.js")
-	}
-
-	return &opts, nil
-}
-
 func main() {
 	log.SetPrefix("sourceindexer.go: ")
 	log.SetFlags(0) // don't print timestamps
 
-	opts, err := parseOptions(os.Args)
+	tmpDir := os.TempDir()
+
+	opts, err := ParseOptions(tmpDir, os.Args[0], os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if opts.debug {
+	if opts.Debug {
 		// log.Println(os.Environ())
 		listFiles(".")
 	}
 
-	args := append([]string{opts.scriptPath}, opts.files...)
-	env := []string{"NODE_PATH=" + opts.nodePath}
+	args := append([]string{opts.ScriptPath}, opts.Files...)
+	env := []string{"NODE_PATH=" + opts.NodePath}
 
-	exitCode, err := run(opts.nodeBinPath, args, ".", env)
+	exitCode, err := run(opts.NodeBinPath, args, ".", env)
 	if err != nil {
 		log.Print(err)
 	}
@@ -134,36 +78,4 @@ func listFiles(dir string) error {
 		log.Println(path)
 		return nil
 	})
-}
-
-// mustRestore - Restore assets.
-func mustRestore(opts *options, baseDir string, assets map[string][]byte) {
-	// unpack variable is provided by the go_embed data and is a
-	// map[string][]byte such as {"/usr/share/games/fortune/literature.dat":
-	// bytes... }
-	for rel, bytes := range assets {
-		filename := filepath.Join(baseDir, rel)
-		dirname := filepath.Dir(filename)
-		// log.Printf("file %s, dir %s, rel %d, abs %s, absdir: %s", file, dir, rel, abs, absdir)
-		if err := os.MkdirAll(dirname, os.ModePerm); err != nil {
-			log.Fatalf("Failed to create asset dir %s: %v", dirname, err)
-		}
-		if err := ioutil.WriteFile(filename, bytes, os.ModePerm); err != nil {
-			log.Fatalf("Failed to write asset %s: %v", filename, err)
-		}
-
-		switch filepath.Base(rel) {
-		case "sourceindexer.js":
-			opts.scriptPath = filename
-		case "node":
-			opts.nodeBinPath = filename
-		case "package.json":
-			// cmd/sourceindexer/node_modules/scalameta-parsers/package.json -> cmd/sourceindexer/
-			opts.nodePath = filepath.Dir(filepath.Dir(dirname))
-		}
-
-		if opts.debug {
-			log.Printf("Restored %s", filename)
-		}
-	}
 }
