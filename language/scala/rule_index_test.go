00edfbc6f1b13410e316793b7b17e4e566a26297
Paul Johnston <pcj@stack.build>
2022-12-02 02:17:55 -0700

    add test skeleton

--- language/scala/rule_index_test.go
+++ language/scala/rule_index_test.go
@@ -1,8 +1,13 @@
 package scala
 
 import (
+	"testing"
+
 	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/bazelbuild/bazel-gazelle/resolve"
 	"github.com/bazelbuild/bazel-gazelle/rule"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stackb/scala-gazelle/pkg/crossresolve"
 )
 
 type mockRuleIndex struct {
@@ -14,4 +19,26 @@ type mockRuleIndex struct {
 func (m *mockRuleIndex) LookupRule(from label.Label) (*rule.Rule, bool) {
 	m.from = from
 	return nil, false
+}
+
+// LookupImport imp resolve.ImportSpec) (*crossresolve.ImportProvider, bool) {
+func (m *mockRuleIndex) LookupImport(imp resolve.ImportSpec) (*crossresolve.ImportProvider, bool) {
+	return nil, false
+}
+
+func TestLookupImport(t *testing.T) {
+	for name, tc := range map[string]struct {
+		imp  resolve.ImportSpec
+		want *crossresolve.ImportProvider
+	}{
+		"degenerate": {},
+	} {
+		t.Run(name, func(t *testing.T) {
+			lang := NewLanguage().(*scalaLang)
+			got, _ := lang.LookupImport(tc.imp)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("(-want +got):\n%s", diff)
+			}
+		})
+	}
 }
