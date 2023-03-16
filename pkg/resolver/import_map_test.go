94822466a1d59bf8fa4f16f0cc628e1f4e260bcf
Paul Johnston <pcj@stack.build>
2023-03-16 15:00:02 -0600

    Add import_map_test.go

---
+++ pkg/resolver/import_map_test.go
@@ -0,0 +1,85 @@
+package resolver_test
+
+import (
+	"testing"
+
+	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/stackb/scala-gazelle/build/stack/gazelle/scala/parse"
+	"github.com/stackb/scala-gazelle/pkg/resolver"
+)
+
+func TestImportMapDeps(t *testing.T) {
+	for name, tc := range map[string]struct {
+		from    label.Label
+		imports []*resolver.Import
+		want    []label.Label
+		wantErr error
+	}{
+		"degenerate": {},
+		"removes duplicates": {
+			imports: []*resolver.Import{
+				{
+					Kind: parse.ImportKind_DIRECT,
+					Imp:  "com.typesafe.scalalogging.LazyLogging",
+					Symbol: &resolver.Symbol{
+						Name:     "com.typesafe.scalalogging.LazyLogging",
+						Provider: "maven",
+						Label:    label.Label{Repo: "maven", Name: "com_typesafe_scalalogging"},
+					},
+				},
+				{
+					Kind: parse.ImportKind_DIRECT,
+					Imp:  "com.typesafe.scalalogging.LazyLogging",
+					Symbol: &resolver.Symbol{
+						Name:     "com.typesafe.scalalogging.LazyLogging",
+						Provider: "maven",
+						Label:    label.Label{Repo: "maven", Name: "com_typesafe_scalalogging"},
+					},
+				},
+			},
+			from: label.Label{Pkg: "app", Name: "server"},
+			want: []label.Label{
+				{Repo: "maven", Name: "com_typesafe_scalalogging"},
+			},
+		},
+		"removes self-imports": {
+			imports: []*resolver.Import{
+				{
+					Kind: parse.ImportKind_DIRECT,
+					Imp:  "com.typesafe.scalalogging.LazyLogging",
+					Symbol: &resolver.Symbol{
+						Name:     "com.typesafe.scalalogging.LazyLogging",
+						Provider: "maven",
+						Label:    label.Label{Repo: "maven", Name: "com_typesafe_scalalogging"},
+					},
+				},
+				{
+					Kind: parse.ImportKind_DIRECT,
+					Imp:  "app.Server",
+					Symbol: &resolver.Symbol{
+						Name:     "app.Server",
+						Provider: "source",
+						Label:    label.Label{Relative: true, Name: "server"},
+					},
+				},
+			},
+			from: label.Label{Pkg: "app", Name: "server"},
+			want: []label.Label{
+				{Repo: "maven", Name: "com_typesafe_scalalogging"},
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			importMap := resolver.NewImportMap()
+			for _, imp := range tc.imports {
+				importMap.Put(imp)
+			}
+			got := importMap.Deps(tc.from)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("(-want +got):\n%s", diff)
+			}
+		})
+	}
+}
