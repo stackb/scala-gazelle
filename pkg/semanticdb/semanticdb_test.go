38d6f25984d2037608670fbfd9da0a4139d7338f
Paul Johnston <pcj@stack.build>
2024-09-12 20:19:14 -0600

    Fix golden test (non-update case)

--- pkg/semanticdb/semanticdb_test.go
+++ pkg/semanticdb/semanticdb_test.go
@@ -16,132 +16,6 @@ import (
 
 var update = flag.Bool("update", false, "update golden files")
 
-func SkipTestReadJarFile(t *testing.T) {
-	for name, tc := range map[string]struct {
-		filename string
-		wantErr  string
-	}{
-		"degenerate": {
-			wantErr: "opening jar file: open : no such file or directory",
-		},
-		"example jar": {
-			filename: "testdata/example.jar",
-		},
-	} {
-		t.Run(name, func(t *testing.T) {
-			dir, err := os.Getwd()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if bwd, ok := os.LookupEnv("BUILD_WORKSPACE_DIRECTORY"); ok {
-				dir = filepath.Join(bwd, "pkg/semanticdb")
-			}
-
-			got, err := ReadJarFile(tc.filename)
-			var gotErr string
-			if err != nil {
-				gotErr = err.Error()
-			}
-			if diff := cmp.Diff(tc.wantErr, gotErr); diff != "" {
-				t.Errorf("error (-want +got):\n%s", diff)
-			}
-			if err != nil {
-				return
-			}
-
-			for _, docs := range got {
-				for _, doc := range docs.Documents {
-					goldenFile := filepath.Join(dir, "testdata", tc.filename, "META-INF", "semanticdb", doc.Uri+".json")
-
-					if *update {
-						if err := os.MkdirAll(filepath.Dir(goldenFile), os.ModePerm); err != nil {
-							t.Fatal(err)
-						}
-						if err := protobuf.WriteStableJSONFile(goldenFile, doc); err != nil {
-							t.Fatal(err)
-						}
-						log.Println("Wrote golden file:", goldenFile)
-						continue
-					}
-
-					var want spb.TextDocument
-					if err := protobuf.ReadFile(goldenFile, &want); err != nil {
-						t.Fatal(err)
-					}
-
-					if diff := cmp.Diff(&want, doc,
-						cmpopts.IgnoreUnexported(
-							spb.TextDocuments{},
-							spb.TextDocument{},
-							spb.Range{},
-							spb.Location{},
-							spb.Scope{},
-							spb.Type{},
-							spb.LambdaType{},
-							spb.TypeRef{},
-							spb.SingleType{},
-							spb.ThisType{},
-							spb.SuperType{},
-							spb.ConstantType{},
-							spb.IntersectionType{},
-							spb.UnionType{},
-							spb.WithType{},
-							spb.StructuralType{},
-							spb.AnnotatedType{},
-							spb.ExistentialType{},
-							spb.UniversalType{},
-							spb.ByNameType{},
-							spb.RepeatedType{},
-							spb.MatchType{},
-							spb.Constant{},
-							spb.UnitConstant{},
-							spb.BooleanConstant{},
-							spb.ByteConstant{},
-							spb.ShortConstant{},
-							spb.CharConstant{},
-							spb.IntConstant{},
-							spb.LongConstant{},
-							spb.FloatConstant{},
-							spb.DoubleConstant{},
-							spb.StringConstant{},
-							spb.NullConstant{},
-							spb.Signature{},
-							spb.ClassSignature{},
-							spb.MethodSignature{},
-							spb.TypeSignature{},
-							spb.ValueSignature{},
-							spb.SymbolInformation{},
-							spb.Documentation{},
-							spb.Annotation{},
-							spb.Access{},
-							spb.PrivateAccess{},
-							spb.PrivateThisAccess{},
-							spb.PrivateWithinAccess{},
-							spb.ProtectedAccess{},
-							spb.ProtectedThisAccess{},
-							spb.ProtectedWithinAccess{},
-							spb.PublicAccess{},
-							spb.SymbolOccurrence{},
-							spb.Diagnostic{},
-							spb.Synthetic{},
-							spb.Tree{},
-							spb.ApplyTree{},
-							spb.FunctionTree{},
-							spb.IdTree{},
-							spb.LiteralTree{},
-							spb.MacroExpansionTree{},
-							spb.OriginalTree{},
-							spb.SelectTree{},
-							spb.TypeApplyTree{},
-						)); diff != "" {
-						t.Errorf("%s (-want +got):\n%s", doc.Uri, diff)
-					}
-				}
-			}
-		})
-	}
-}
-
 func TestSemanticImports(t *testing.T) {
 	for name, tc := range map[string]struct {
 		filename string
@@ -150,6 +24,9 @@ func TestSemanticImports(t *testing.T) {
 		"stringlib": {
 			filename: "testdata/stringlib.jar.textdocuments.json",
 		},
+		"euds": {
+			filename: "testdata/edus.jar.textdocuments.json",
+		},
 	} {
 		t.Run(name, func(t *testing.T) {
 			dir, err := os.Getwd()
@@ -160,8 +37,8 @@ func TestSemanticImports(t *testing.T) {
 				dir = filepath.Join(bwd, "pkg/semanticdb")
 			}
 
-			var got spb.TextDocuments
-			err = protobuf.ReadFile(tc.filename, &got)
+			var docs spb.TextDocuments
+			err = protobuf.ReadFile(tc.filename, &docs)
 			var gotErr string
 			if err != nil {
 				gotErr = err.Error()
@@ -173,10 +50,8 @@ func TestSemanticImports(t *testing.T) {
 				return
 			}
 
-			for _, doc := range got.Documents {
-				semanticImports := SemanticImports(doc)
-				file := new(sppb.File)
-				file.SemanticImports = semanticImports
+			for _, doc := range docs.Documents {
+				got := sppb.File{SemanticImports: SemanticImports(doc)}
 
 				goldenFile := filepath.Join(dir, "testdata", tc.filename+".golden", doc.Uri+".file.json")
 
@@ -184,7 +59,7 @@ func TestSemanticImports(t *testing.T) {
 					if err := os.MkdirAll(filepath.Dir(goldenFile), os.ModePerm); err != nil {
 						t.Fatal(err)
 					}
-					if err := protobuf.WriteStableJSONFile(goldenFile, file); err != nil {
+					if err := protobuf.WriteStableJSONFile(goldenFile, &got); err != nil {
 						t.Fatal(err)
 					}
 					log.Println("Wrote golden file:", goldenFile)
@@ -196,10 +71,9 @@ func TestSemanticImports(t *testing.T) {
 					t.Fatal(err)
 				}
 
-				if diff := cmp.Diff(&want, doc,
-					cmpopts.IgnoreUnexported(
-						sppb.File{},
-					)); diff != "" {
+				if diff := cmp.Diff(&want, &got, cmpopts.IgnoreUnexported(
+					sppb.File{},
+				)); diff != "" {
 					t.Errorf("%s (-want +got):\n%s", doc.Uri, diff)
 				}
 			}
