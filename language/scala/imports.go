323b28766b17e5ca213a883034b29821dd59a403
Paul Johnston <pcj@stack.build>
2024-09-22 14:37:02 -0600

    Add .txt file output format

--- language/scala/imports.go
+++ language/scala/imports.go
@@ -1,8 +1,12 @@
 package scala
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/bazelbuild/bazel-gazelle/label"
 	scpb "github.com/stackb/scala-gazelle/build/stack/gazelle/scala/cache"
 	"github.com/stackb/scala-gazelle/pkg/protobuf"
 )
@@ -13,11 +17,28 @@ func (sl *scalaLang) writeResolvedImportsMapFile(filename string) error {
 	}
 
 	for _, sym := range sl.globalScope.GetSymbols("") {
-		imports.Imports[sym.Name] = sym.Label.String()
+		dep := "NO_LABEL"
+		if sym.Label != label.NoLabel {
+			dep = sym.Label.String()
+		}
+		imports.Imports[sym.Name] = dep
 	}
 
-	if err := protobuf.WriteFile(filename, imports); err != nil {
-		return err
+	if filepath.Ext(filename) == ".txt" {
+		f, err := os.Create(filename)
+		if err != nil {
+			return fmt.Errorf("create: %w", err)
+		}
+		for k, v := range imports.Imports {
+			fmt.Fprintln(f, k, v)
+		}
+		if err := f.Close(); err != nil {
+			return fmt.Errorf("close: %w", err)
+		}
+	} else {
+		if err := protobuf.WriteFile(filename, imports); err != nil {
+			return err
+		}
 	}
 
 	log.Printf("Wrote scala-gazelle import map %s", filename)
