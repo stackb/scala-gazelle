3dca12d3d0b58f4216510a5bc1eecc5b90c108d8
Paul Johnston <pcj@stack.build>
2025-03-20 12:52:45 -0600

    Initial setup for package_filegroup

--- language/scala/golden_test.go
+++ language/scala/golden_test.go
@@ -14,12 +14,13 @@ func TestScala(t *testing.T) {
 	}
 	goldentest.FromDir("language/scala",
 		goldentest.WithOnlyTests(
-			"java_provider",
-			"maven_provider",
-			"override_provider",
-			"protobuf_provider",
-			"resolve_kind_rewrite_name",
-			"source_provider",
+			// "java_provider",
+			// "maven_provider",
+			// "override_provider",
+			// "protobuf_provider",
+			// "resolve_kind_rewrite_name",
+			// "source_provider",
+			"scala_fileset",
 		),
 	).Run(t, "gazelle")
 }
